4a1d0b6b0921a1eb0be2cc34260f7ebd04d78653
emilasbringer <emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.se>
2022-03-18 11:19:00 +0100

    🚧  Mysql queries from meep-page

--- routes/meeps.js
+++ routes/meeps.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET Meep page. */
-router.get('/', function(req, res, next) {
-    res.render('meeps.njk', {layout: "layout.njk", title: 'Express' });
-  });
+const express = require('express');
+const { response } = require('../app');
+const router = express.Router();
+const pool = require('../database');
+/* 
+    BASE URL /tasks
+    GET / - Get all tasks
+    POST / - Create a new task
+    GET /:id - Get a task by id
+    PUT /:id - Update a task by id
+    DELETE /:id - Delete a task by id
+*/
+router.get('/', async (req, res, next) => {
+    await pool.promise()
+        .query('SELECT * FROM meeps')
+        .then(([rows, fields]) => {
+              res.send('showing meeps', {
+                tasks: rows,
+                title: 'Meeps',
+                layout: 'layout.njk',
+              });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting tasks'
+                }
+            })
+        });
+});
 
 module.exports = router;
