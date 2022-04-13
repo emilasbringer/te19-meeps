2faab4776adf8bdf8e882fcfd0e4f8c8461edab8
emilasbringer <emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.se>
2022-04-13 12:43:10 +0200

    api route

--- routes/api.js
+++ routes/api.js
@@ -18,4 +18,36 @@ router.get('/', async (req, res, next) => {
         });
 });
 
+router.get('/body', async (req, res, next) => {
+    await pool.promise()
+        .query('SELECT * FROM emlasr_meeps JOIN emlasr_users WHERE user_id = emlasr_users.id ORDER BY created_at DESC')
+        .then(([rows, fields]) => {
+            res.send(rows.body);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting meeps'
+                }
+            })
+        });
+});
+
+router.get('/users', async (req, res, next) => {
+    await pool.promise()
+        .query('SELECT * FROM emlasr_meeps JOIN emlasr_users WHERE user_id = emlasr_users.id ORDER BY created_at DESC')
+        .then(([rows, fields]) => {
+            res.send(rows.name);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting meeps'
+                }
+            })
+        });
+});
+
 module.exports = router;
